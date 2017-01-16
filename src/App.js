43b497f1a34c1c5809e5fad3c69ab4549ba32e5d
naneen <neen_tta@hotmail.com>
2017-01-16 20:26:52 +0700

    edit code color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button bsStyle="primary">test</Button>
+        <Button>no bsStyle (default)</Button>
+        <Button bsStyle="primary">bsStyle="primary"</Button>
         <div className="container">
           {this.props.children}
         </div>
