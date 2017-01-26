e316a0018998063ea8e33b0071aba2b3e3209b3c
tanDKY <tan.dooky@gmail.com>
2017-01-26 14:29:30 +0700

    check

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Login from './Login';
-import PeopleContainer from './People/PeopleContainer';
 import Register from './Register';
 import FeaturePlanningContainer from './FeaturePlanning/FeaturePlanningContainer';
 import EffortEstimationContainer from './EffortEstimation/Container';
@@ -7,7 +6,6 @@ import EffortEstimationContainer from './EffortEstimation/Container';
 module.exports = {
   Login,
   Register,
-  PeopleContainer,
   FeaturePlanningContainer,
   EffortEstimationContainer,
 };
