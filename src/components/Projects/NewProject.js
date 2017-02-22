8d40c7f0aa5c42c9fc877cb2655740a7321e8bd7
naneen <neen_tta@hotmail.com>
2017-02-22 13:41:51 +0700

    finish newProject

--- src/components/Projects/NewProject.js
+++ src/components/Projects/NewProject.js
@@ -8,8 +8,33 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import linkState from 'react-link-state';
 import axios from 'axios';
+import Autosuggest from 'react-autosuggest';
 import logo from '../../images/logo-login.png';
 import * as projectActions from '../../actions/project-actions';
+import autosuggestStyle from '../../style/autosuggestStyle.css';
+
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestionValue(suggestion) {
+  return `${suggestion.username} ${suggestion.email}`;
+}
+
+function renderSuggestion(suggestion) {
+  const suggestionText = `${suggestion.username} (${suggestion.email})`;
+  return (
+    <span className={'suggestion-content ' + suggestion.image}>
+      <span className="name">
+        <span>{suggestionText}</span>
+      </span>
+    </span>
+  );
+}
+
+Array.prototype.diff = function(a) {
+  return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
 
 class NewProject extends Component {
 
@@ -21,16 +46,33 @@ class NewProject extends Component {
         name: '',
         description: '',
         sprint_duration: '',
-        users: [
-          {
-            id: 2,
-          },
-        ],
+        users: [],
         organization_id: this.props.params.organizationId,
       },
+      allUsers: [],
+      value: '',
+      suggestions: [],
+      contributors: [],
     };
 
     this.create = this.create.bind(this);
+  }
+
+  componentWillMount() {
+    axios({
+      method: 'GET',
+      url: '/api/users',
+      params: {
+        organization: this.props.params.organizationId,
+      },
+    }).then((response) => {
+      const users = response.data
+      this.setState({
+        allUsers: users,
+      })
+    }).catch((error) => {
+      console.log(error.response);
+    });
   }
 
   create() {
@@ -51,6 +93,90 @@ class NewProject extends Component {
     }).catch((error) => {
       console.log(error);
     });
+  }
+
+  getSuggestions(value) {
+    const inputValue = escapeRegexCharacters(value.trim().toLowerCase());
+    if (inputValue === '') {
+      return [];
+    }
+    const availableUsers = this.state.allUsers.diff(this.state.contributors);
+    const regex = new RegExp('\\b' + inputValue, 'i');
+    return availableUsers.filter(person => regex.test(getSuggestionValue(person)));
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value),
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+  onSuggestionSelected = (event, { suggestion }) => {
+    if (suggestion !== undefined) {
+      const newUsernames = this.state.contributors.slice();
+      const newUsers = this.state.input.users.slice();
+      newUsernames.push(suggestion);
+      newUsers.push({ id: suggestion.id });
+      const newInput = this.state.input;
+      newInput.users = newUsers;
+      this.setState({
+        contributors: newUsernames,
+        value: '',
+        input: newInput,
+      });
+    }
+  }
+
+  removeContributor(id) {
+    const array = this.state.contributors;
+    const index = array.indexOf(id);
+    array.splice(index, 1);
+
+    const users = this.state.input.users;
+    const uindex = users.indexOf(id);
+
+    const newInput = this.state.input;
+    users.splice(uindex, 1);
+    newInput.users = users;
+
+    this.setState({
+      contributors: array,
+      input: newInput,
+    });
+  }
+
+  contributor() {
+    const content = this.state.contributors.map((contributor) =>
+      <Row key={contributor.id}>
+        <Col smOffset={0} xs={9} md={10}>
+          <span className={'suggestion-content ' + contributor.image}>
+            <span className="name">
+              <span>{contributor.username}</span>
+            </span>
+          </span>
+        </Col>
+        <Col smOffset={0} xs={2} md={2}>
+          <Button bsStyle="primary" onClick={() => this.removeContributor(contributor.id)}><Glyphicon glyph="remove" /></Button>
+        </Col>
+      </Row>
+    );
+    return (
+      <div>
+        {content}
+      </div>
+    );
   }
 
   render() {
@@ -75,10 +201,19 @@ class NewProject extends Component {
       borderLeft: '1px solid #7E8281',
       position: 'absolute',
       height: '85%',
-      width: 'auto',
-      overflowY: 'hidden',
-      overflowX: 'hidden',
+      width: '100%',
+      overflowY: 'auto',
+      overflowX: 'auto',
     };
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'Find user',
+      value,
+      onChange: this.onChange,
+      onClick: this.onSuggestionSelected,
+    };
+    const previousURL = "/organizations/".concat(this.state.input.organizationId);
+
     return (
       <div>
         <Grid>
@@ -116,20 +251,20 @@ class NewProject extends Component {
               </Col>
             </Row>
             <Row>
-              <Col xs={12} md={4} xsOffset={0} mdOffset={2}>
+              <Col xs={12} md={4} mdOffset={2}>
                 <FormGroup controlId="formInlineContributor">
                   <ControlLabel>
                     Contributor
                   </ControlLabel>
-                  <InputGroup>
-                    <FormControl
-                      placeholder="Find user"
-                      valueLink={linkState(this, 'input.u')}
-                    />
-                    <InputGroup.Addon>
-                      <Glyphicon glyph="plus" />
-                    </InputGroup.Addon>
-                  </InputGroup>
+                  <Autosuggest
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={inputProps}
+                    onSuggestionSelected={this.onSuggestionSelected}
+                  />
                 </FormGroup>
               </Col>
               <Col style={contributorList} xs={12} md={4}>
@@ -138,22 +273,7 @@ class NewProject extends Component {
                 </ControlLabel>
                 <Row style={scrollableContainer}>
                   <Col smOffset={0} sm={11}>
-                    <Media>
-                      <Media.Left>
-                        <Image width={32} height={32} src={logo} alt="user" circle />
-                      </Media.Left>
-                      <Media.Body>
-                        <p>pongsachon.p@ku.th</p>
-                      </Media.Body>
-                    </Media>
-                    <Media>
-                      <Media.Left>
-                        <Image width={32} height={32} src={logo} alt="user" circle />
-                      </Media.Left>
-                      <Media.Body>
-                        <p>pongsachon.p@ku.th</p>
-                      </Media.Body>
-                    </Media>
+                    {this.contributor()}
                   </Col>
                 </Row>
               </Col>
@@ -161,12 +281,12 @@ class NewProject extends Component {
             <Row>
               <FormGroup style={buttonGroup}>
                 <Col xs={12} md={3} xsOffset={0} mdOffset={3}>
-                  <Button style={singleButton} bsStyle="primary" href="/no-organization" block>
+                  <Button style={singleButton} bsStyle="primary" href={previousURL} key="cancel" block>
                     Cancel
                   </Button>
                 </Col>
                 <Col xs={12} md={3}>
-                  <Button style={singleButton} onClick={this.create} block>
+                  <Button style={singleButton} onClick={this.create} key="submitProject" block>
                     Create
                   </Button>
                 </Col>
