6747a21779a9c9733b5642668cd0fc31c87b290b
Mintra Thirasirisin <mntrqq@gmail.com>
2017-03-14 15:03:14 +0700

    Clean up code

--- src/components/Backlog/BacklogContainer.js
+++ src/components/Backlog/BacklogContainer.js
@@ -1,10 +1,11 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
-import linkState from 'react-link-state';
 import { bindActionCreators } from 'redux';
-import { Row, Col, Modal, Button, Form, FormGroup, FormControl, Grid, ControlLabel } from 'react-bootstrap';
+import update from 'immutability-helper';
+import EditTaskModal from './EditTaskModal';
+import { Row, Col, Button } from 'react-bootstrap';
 import 'simple-line-icons/css/simple-line-icons.css';
-import Style from '../../style/backlog.css';
+import '../../style/backlog.css';
 import * as apiHelper from '../../helpers/apiHelper';
 import * as backlogActions from '../../actions/backlog-actions';
 
@@ -13,9 +14,9 @@ class BacklogContainer extends Component {
     super(props);
     this.state = {
       show: false,
-      taskID: -1,
       backlogTasks: [],
       sprintTasks: [],
+      task: {},
     };
 
     this.close = this.close.bind(this);
@@ -63,52 +64,46 @@ class BacklogContainer extends Component {
     this.setState({ show: false });
   }
 
-  showEditTaskModal(e) {
-    console.log(e.target);
-    const id = Number(e.target.dataset.value);
+  showEditTaskModal(tempTask) {
     this.setState({
       show: true,
-      taskID: id,
+      task: tempTask,
     });
   }
 
   findIndex(array, id) {
     for (let i = 0; i < array.length; i += 1) {
       if (array[i].id === id) {
-        console.log(`Index found: ${i}`);
         return i;
       }
     }
     return -1;
   }
 
-  switchTask(e) {
-    console.log(e.target);
-    const id = Number(e.target.dataset.value);
-    const btn = e.target.id;
-    const tempBacklogTasks = this.state.backlogTasks;
-    const tempSprintTasks = this.state.sprintTasks;
-
-    if (btn === 'addButton') {
-      const index = this.findIndex(tempBacklogTasks, id);
-      this.setState({ sprintTasks: this.state.sprintTasks.concat(tempBacklogTasks[index]) });
-      tempBacklogTasks.splice(index, 1);
-      this.setState({ backlogTasks: tempBacklogTasks });
-    } else if (btn === 'removeButton') {
-      const index = this.findIndex(tempSprintTasks, id);
-      this.setState({ backlogTasks: this.state.backlogTasks.concat(tempSprintTasks[index]) });
-      tempSprintTasks.splice(index, 1);
-      this.setState({ sprintTasks: tempSprintTasks });
+  switchTask(tempTask, btn) {
+    if (btn === 'add') {
+      const index = this.findIndex(this.state.backlogTasks, tempTask.id);
+      this.setState({
+        sprintTasks: update(this.state.sprintTasks, { $push: [tempTask] }),
+        backlogTasks: update(this.state.backlogTasks, { $splice: [[index, 1]] }),
+      });
+    } else {
+      const index = this.findIndex(this.state.sprintTasks, tempTask.id);
+      this.setState({
+        backlogTasks: update(this.state.backlogTasks, { $push: [tempTask] }),
+        sprintTasks: update(this.state.sprintTasks, { $splice: [[index, 1]] }),
+      });
     }
   }
 
   render() {
     const backlogTaskNode = this.state.backlogTasks.map((task) => {
       return (
-        <Row>
-          <li key={task.id} id="task">
-            <Col xs={10} md={11}><span id="taskName" data-value={task.id} onClick={this.showEditTaskModal}>{task.name}</span></Col>
-            <Col xs={1} md={1}><span className="icon-plus" data-value={task.id} id="addButton" onClick={this.switchTask} /></Col>
+        <Row key={task.id}>
+          <li id="task">
+            <Col xs={10} md={11}>
+              <span id="taskName" onClick={() => this.showEditTaskModal(task)}>{task.name}</span></Col>
+            <Col xs={1} md={1}><span className="icon-plus" id="addButton" onClick={() => this.switchTask(task, 'add')} /></Col>
           </li>
         </Row>
       );
@@ -116,10 +111,10 @@ class BacklogContainer extends Component {
 
     const sprintTaskNode = this.state.sprintTasks.map((task) => {
       return (
-        <Row>
-          <li key={task.id} id="task">
-            <Col xs={10} md={10}><span id="taskName" data-value={task.id} onClick={this.showEditTaskModal}>{task.name}</span></Col>
-            <Col xs={1} md={1}><span className="icon-close" data-value={task.id} role="button" id="removeButton" onClick={this.switchTask}/></Col>
+        <Row key={task.id}>
+          <li id="task">
+            <Col xs={10} md={10}><span id="taskName" onClick={() => this.showEditTaskModal(task)}>{task.name}</span></Col>
+            <Col xs={1} md={1}><span className="icon-close" role="button" id="removeButton" onClick={() => this.switchTask(task, 'remove')} /></Col>
           </li>
         </Row>
       );
@@ -132,72 +127,9 @@ class BacklogContainer extends Component {
       return (<Button className="disabled">Next</Button>);
     };
 
-    const editTask = () => {
-      let task = {};
-      let i = -1;
-      if (this.findIndex(this.state.backlogTasks, this.state.taskID) !== -1) {
-        i = this.findIndex(this.state.backlogTasks, this.state.taskID);
-        task = this.state.backlogTasks[i];
-      } else if (this.findIndex(this.state.sprintTasks, this.state.taskID) !== -1) {
-        i = this.findIndex(this.state.sprintTasks, this.state.taskID);
-        task = this.state.sprintTasks[i];
-      }
-      return (
-        <div className="editTaskContainer">
-          <Form>
-            <Row>
-              <hr />
-              <FormGroup controlId="editName">
-                <ControlLabel>
-                  Task
-                </ControlLabel>
-                <FormControl type="text" value={task.name} />
-              </FormGroup>
-            </Row>
-            <Row>
-              <FormGroup controlId="editDescription">
-                <ControlLabel>
-                  Description
-                </ControlLabel>
-                <FormControl type="text" value={task.description} />
-              </FormGroup>
-            </Row>
-            <Row>
-              <Col md={5} id="leftColumn">
-                <FormGroup controlId="">
-                  <ControlLabel>
-                    Feature
-                  </ControlLabel>
-                  <FormControl type="text" />
-                </FormGroup>
-              </Col>
-              <Col md={5} mdOffset={2} id="rightColumn">
-                <FormGroup controlId="">
-                  <ControlLabel>
-                    Assignee
-                  </ControlLabel>
-                  <FormControl type="text" />
-                </FormGroup>
-              </Col>
-            </Row>
-            <Row>
-              <Col md={5} id="leftColumn">
-                <FormGroup controlId="">
-                  <ControlLabel>
-                    Tag
-                  </ControlLabel>
-                  <FormControl type="text" />
-                </FormGroup>
-              </Col>
-            </Row>
-          </Form>
-        </div>
-      );
-    };
-
     return (
       <div className="modal-container">
-        <div className="backlogContainer" style={Style}>
+        <div className="backlogContainer">
           <Row>
             <Col sm={8}>
               <h4>Backlog</h4>
@@ -212,22 +144,10 @@ class BacklogContainer extends Component {
             </Col>
           </Row>
         </div>
-        <Modal
+        <EditTaskModal
+          task={this.state.task}
           show={this.state.show}
-          onHide={this.close}
-          container={this}
-          aria-labelledby="contained-modal-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title">Task</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {editTask()}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.close}>Update</Button>
-          </Modal.Footer>
-        </Modal>
+        />
       </div>
     );
   }
---
+++ src/components/Backlog/EditTaskModal.js
@@ -0,0 +1,107 @@
+import React, { PropTypes, Component } from 'react';
+import update from 'immutability-helper';
+import { Row, Col, Modal, Button, Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import '../../style/backlog.css';
+
+class EditTaskModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: this.props.task,
+    };
+    this.handleEditTask = this.handleEditTask.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ input: nextProps.task });
+  }
+
+  handleEditTask(e) {
+    const name = e.target.name;
+    this.setState({ input: update(this.state.input, { [name]: { $set: e.target.value } }) });
+  }
+
+  render() {
+    const editTask = () => {
+      return (
+        <div className="editTaskContainer">
+          <Form>
+            <Row>
+              <hr />
+              <FormGroup>
+                <ControlLabel>
+                  Task
+                </ControlLabel>
+                <FormControl type="text" name="name" onChange={this.handleEditTask} value={this.state.input.name} />
+              </FormGroup>
+            </Row>
+            <Row>
+              <FormGroup>
+                <ControlLabel>
+                  Description
+                </ControlLabel>
+                <FormControl type="text" name="description" onChange={this.handleEditTask} value={this.state.input.description} />
+              </FormGroup>
+            </Row>
+            <Row>
+              <Col md={5} id="leftColumn">
+                <FormGroup>
+                  <ControlLabel>
+                    Feature
+                  </ControlLabel>
+                  <FormControl type="text" />
+                </FormGroup>
+              </Col>
+              <Col md={5} mdOffset={2} id="rightColumn">
+                <FormGroup controlId="assignee">
+                  <ControlLabel>
+                    Assignee
+                  </ControlLabel>
+                  <FormControl type="text" />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={5} id="leftColumn">
+                <FormGroup controlId="tag">
+                  <ControlLabel>
+                    Tag
+                  </ControlLabel>
+                  <FormControl type="text" />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      );
+    };
+
+    return (
+      <div className="modal-container">
+        <Modal
+          show={this.props.show}
+          onHide={this.close}
+          container={this}
+          aria-labelledby="contained-modal-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title">Task</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {editTask()}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Update</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+EditTaskModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  // task: PropTypes.object.isReqired,
+};
+
+export default EditTaskModal;
