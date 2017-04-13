f7cb59ec85db52ff429101c2d9e0feee9ea17531
naneen <neen_tta@hotmail.com>
2017-04-13 16:17:19 +0700

    add chart's option

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -1,38 +1,42 @@
 import React, { Component, PropTypes } from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 // import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 
 class DashboardContainer extends Component {
   render() {
     const data = {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      }],
+      labels: ['Item 1', 'Item 2', 'Item 3'],
+      datasets: [
+        {
+          type: 'bar',
+          label: 'Bar Component',
+          data: [10, 20, 30],
+        },
+        {
+          type: 'line',
+          label: 'Line Component',
+          data: [30, 20, 10],
+          borderColor: '#F85F73',
+        },
+      ],
+    };
+    const options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: 'true',
+            // max: 5,
+            // min: 0,
+            // stepSize: 0.5,
+          },
+        }],
+      },
     };
     return (
       <div>
         <h1>Hello</h1>
-        <Doughnut data={data} />
+        <Bar data={data} options={options} />
       </div>
     );
   }
