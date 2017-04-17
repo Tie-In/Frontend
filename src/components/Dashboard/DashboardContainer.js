bee11b85f0a3d30e4b77858cc06d6234889534e0
naneen <neen_tta@hotmail.com>
2017-04-17 10:43:40 +0700

    non curved line chart & combine 2 labels

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -1,42 +1,124 @@
 import React, { Component, PropTypes } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 // import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 
 class DashboardContainer extends Component {
   render() {
+    // const data = {
+    //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    //   datasets: [{
+    //     label: 'Sales',
+    //     type: 'line',
+    //     data: [51, 65, 40, 49, 60, 37, 40],
+    //     fill: false,
+    //     borderColor: '#EC932F',
+    //     backgroundColor: '#EC932F',
+    //     pointBorderColor: '#EC932F',
+    //     pointBackgroundColor: '#EC932F',
+    //     pointHoverBackgroundColor: '#EC932F',
+    //     pointHoverBorderColor: '#EC932F',
+    //     yAxisID: 'y-axis-2',
+    //   },
+    //   {
+    //     type: 'bar',
+    //     label: 'Visitor',
+    //     data: [200, 185, 590, 621, 250, 400, 95],
+    //     fill: false,
+    //     backgroundColor: '#71B37C',
+    //     borderColor: '#71B37C',
+    //     hoverBackgroundColor: '#71B37C',
+    //     hoverBorderColor: '#71B37C',
+    //     yAxisID: 'y-axis-1',
+    //   }],
+    // };
     const data = {
-      labels: ['Item 1', 'Item 2', 'Item 3'],
-      datasets: [
-        {
-          type: 'bar',
-          label: 'Bar Component',
-          data: [10, 20, 30],
-        },
-        {
-          type: 'line',
-          label: 'Line Component',
-          data: [30, 20, 10],
-          borderColor: '#F85F73',
-        },
-      ],
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'My First dataset',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: 'My Second dataset',
+        fillColor: 'rgba(151,187,205,0.2)',
+        strokeColor: 'rgba(151,187,205,1)',
+        pointColor: 'rgba(151,187,205,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151,187,205,1)',
+        data: [28, 48, 40, 19, 86, 27, 90],
+      }],
     };
+
     const options = {
+      responsive: true,
+      // bezierCurve: false,
+      tooltips: {
+        mode: 'label',
+      },
+      elements: {
+        line: {
+          // fill: false,
+          tension: 0,
+        },
+      },
       scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: 'true',
-            // max: 5,
-            // min: 0,
-            // stepSize: 0.5,
+        xAxes: [
+          {
+            display: true,
+            gridLines: {
+              display: false,
+            },
+            labels: {
+              show: true,
+            },
           },
-        }],
+        ],
+        yAxes: [
+          {
+            type: 'linear',
+            display: true,
+            position: 'left',
+            id: 'y-axis-1',
+            gridLines: {
+              display: false,
+            },
+            labels: {
+              show: true,
+            },
+          },
+          // {
+          //   type: 'linear',
+          //   display: true,
+          //   position: 'right',
+          //   id: 'y-axis-2',
+          //   gridLines: {
+          //     display: false,
+          //   },
+          //   labels: {
+          //     show: true,
+          //   },
+          // },
+        ],
       },
     };
     return (
+      // <div>
+      //   <h1>Hello</h1>
+      //   <Line data={data} options={options} />
+      // </div>
       <div>
-        <h1>Hello</h1>
-        <Bar data={data} options={options} />
+        <h2>Mixed data Example</h2>
+        <Line
+          data={data}
+          options={options}
+        />
       </div>
     );
   }
