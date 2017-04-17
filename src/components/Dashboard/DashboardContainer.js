7f06c0a5644631219852c1b375f8ffa2eeb6897e
naneen <neen_tta@hotmail.com>
2017-04-17 21:05:17 +0700

    save

--- src/components/Dashboard/DashboardContainer.js
+++ src/components/Dashboard/DashboardContainer.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Line, Bar, Pie } from 'react-chartjs-2';
+import { Line, Pie, Bar } from 'react-chartjs-2';
 import { Col } from 'react-bootstrap';
 import DocumentTitle from 'react-document-title';
 import FaBarChart from 'react-icons/lib/fa/bar-chart';
@@ -9,21 +9,36 @@ import * as apiHelper from '../../helpers/apiHelper';
 import * as projectActionsCreator from '../../actions/project-actions';
 
 class DashboardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: this.props.project,
+      sprints: this.props.project.sprints,
+      tasks: '',
+    };
+    this.getDates = this.getDates.bind(this);
+  }
 
   async componentWillMount() {
-    const { params, projectActions } = this.props;
     try {
-      const response = await apiHelper.get(`/api/projects/${params.projectId}`);
-      const project = response.data;
-      projectActions.setProject(project);
+      const sprint = this.state.sprints.slice(-1);
+      const sprintResponse = await apiHelper.get('/api/tasks', {
+        project: this.props.project.id,
+        sprint: sprint[0].id,
+      });
+      this.setState({ tasks: sprintResponse.data });
     } catch (err) {
       console.log(err);
     }
   }
 
+  getDates() {
+    return ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  }
+
   render() {
     const data1 = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: this.getDates,
       datasets: [{
         label: 'Sales',
         type: 'line',
@@ -35,10 +50,10 @@ class DashboardContainer extends Component {
         pointBackgroundColor: '#EC932F',
         pointHoverBackgroundColor: '#EC932F',
         pointHoverBorderColor: '#EC932F',
-        yAxisID: 'y-axis-2',
+        yAxisID: 'y-axis-1',
       },
       {
-        type: 'bar',
+        type: 'line',
         label: 'Visitor',
         data: [200, 185, 590, 621, 250, 400, 95],
         fill: false,
@@ -78,18 +93,6 @@ class DashboardContainer extends Component {
             display: true,
             position: 'left',
             id: 'y-axis-1',
-            gridLines: {
-              display: false,
-            },
-            labels: {
-              show: true,
-            },
-          },
-          {
-            type: 'linear',
-            display: true,
-            position: 'right',
-            id: 'y-axis-2',
             gridLines: {
               display: false,
             },
@@ -154,7 +157,6 @@ class DashboardContainer extends Component {
     const thisPage = {
       color: 'black',
     };
-    console.log(project);
     return (
       <DocumentTitle title={`${project.name}・Dashboard`}>
         <div>
@@ -166,7 +168,7 @@ class DashboardContainer extends Component {
           </Col>
           <Col xs={12} md={6} style={columnStyle}>
             <h4 style={thisPage}><FaBarChart /> Burndown Chart</h4>
-            <Bar data={data1} options={options1} />
+            <Line data={data1} options={options1} />
           </Col>
           <Col xs={12} md={6} style={columnStyle}>
             <h4 style={thisPage}><FaBarChart /> Burndown Chart</h4>
@@ -184,8 +186,6 @@ class DashboardContainer extends Component {
 
 DashboardContainer.propTypes = {
   project: PropTypes.object.isRequired,
-  projectActions: PropTypes.object.isRequired,
-  params: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
