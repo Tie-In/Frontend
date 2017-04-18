669dc4b36ae6d1f01ea6dda20e5219d20946e663
tanDKY <tan.dooky@gmail.com>
2017-04-18 17:20:52 +0700

    check loading in project and org

--- src/components/Projects/ProjectHome.js
+++ src/components/Projects/ProjectHome.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import ReactTooltip from 'react-tooltip';
 import DocumentTitle from 'react-document-title';
 import * as projectActionsCreator from '../../actions/project-actions';
 import * as permissionActionsCreator from '../../actions/permission-actions';
@@ -33,22 +34,21 @@ class ProjectHome extends Component {
     }
   }
 
-  description(text) {
-    return text;
-  }
-
   render() {
     const { project } = this.props;
     const imgStyle = {
       marginRight: '7px',
     };
+    const description = (text) => {
+      return text;
+    };
     return (
+      this.state.loading ? <div /> :
       <DocumentTitle title={`${project.name}`}>
-        { this.state.loading ? <div /> :
         <div className="tiein-container">
           <h3 className="header-label">{project.name}</h3>
           <hr className="header-line" />
-          <p>{this.description(project.description)}</p>
+          <p>{description(project.description)}</p>
           <br />
           <div>
             {
@@ -57,13 +57,15 @@ class ProjectHome extends Component {
               })) : <div />
             }
           </div>
-          {project.effort_estimation === undefined ?
-            <LinkContainer to={`/organizations/${this.props.project.organization_id}/projects/${this.props.project.id}/planning/features`}>
-              <Button>Start Planning </Button>
-            </LinkContainer> : null
-          }
+          <div>
+            {project.effort_estimation === undefined ?
+              <LinkContainer to={`/organizations/${this.props.project.organization_id}/projects/${this.props.project.id}/planning/features`}>
+                <Button>Start Planning </Button>
+              </LinkContainer> : null
+            }
+          </div>
+          <ReactTooltip effect="solid" place="bottom" />
         </div>
-        }
       </DocumentTitle>
     );
   }
